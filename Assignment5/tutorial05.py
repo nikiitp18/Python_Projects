d09f1c7e2173acea58e9ec702d4b149c5ea3a5ad
Nikhil Kumar <nk14646@gmail.com>
2020-11-16 20:58:00 +0530

    Finally, God of all webseries GOT renmaer has been implement, Go and have fun!!

--- Assignment5/tutorial05.py
+++ Assignment5/tutorial05.py
@@ -105,6 +105,35 @@ def rename_FIR(series_selected):
 		except:
 			os.remove(each_dir) 
 
+# Game of Thrones seires episode renaming logic is implemented
+
+def rename_GOT(series_selected):
+	webseries_file = os.path.join('Subtitles',series_selected)
+	director_list = os.listdir(webseries_file)
+	for each_dir in director_list:
+		try:
+			# seprating using regular expression
+			name_in_parts =  re.split('-',each_dir)
+			# segregating season and episode number
+			season_number=re.findall(r'\d+',name_in_parts[1])[0]
+			episode_number=re.findall(r'\d+',name_in_parts[1])[1]
+			# season particular have been restored
+			name_of_episode = (re.split(r'\.',name_in_parts[2])[0]).strip()
+			used_extension = (re.split(r'\.',name_in_parts[2])[-1]).strip()
+
+			webseries_file = os.path.join(os.getcwd(),webseries_file)
+			while(len(episode_number) < int(selected_episode)):
+				episode_number = '0' + episode_number
+			while(len(season_number) < int(selected_season)):
+				season_number = '0' + season_number
+
+			os.chdir(webseries_file)
+			# renaming of file is done here
+			os.rename(each_dir,'Game of Thrones- Season '+ season_number + ' Episode ' + episode_number + '- ' + name_of_episode + '.' + used_extension)  
+		except:
+			os.remove(each_dir)	
+
+
 
 # webseries to be choose, list of webseries is displayed
 webseries_choice = input("Webseries we have in our system:\n1. FIR {FIR}\n2. Game of Thrones {GOT}\n3. How I Met Your Mother {HIMYM}\n4. Sherlock {SLK}\n5. Suits {SUT}\nEnter the code of the webseries which you want ot rename {written inside curly braces}: \n")
@@ -127,7 +156,9 @@ elif webseries_choice == "SUT":
 elif webseries_choice == "SLK":
 	rename_Sherlock(season['SLK'])
 elif webseries_choice == "FIR":
-	rename_FIR(season['FIR'])	
+	rename_FIR(season['FIR'])
+elif webseries_choice == "GOT":
+	rename_GOT(season['GOT'])
 else:
     print(f"\nwarning:  make sure to write the code of the webseries as given. \ninvalid choice!! webseires with code '{webseries_choice}' doesn't exist in our system.")    
 
