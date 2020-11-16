eba46ea91ccd1b8ed4de59e8311eb6a1d02bec72
Nikhil Kumar <nk14646@gmail.com>
2020-11-16 20:37:17 +0530

    FIR, Professor's favourtie series renamer is added. Go check it!!.

--- Assignment5/tutorial05.py
+++ Assignment5/tutorial05.py
@@ -79,7 +79,32 @@ def rename_Sherlock(series_selected):
 			# renaming of file is done here
 			os.rename(each_dir,'Sherlock- Season '+ season_number + ' Episode ' + episode_number + '.' + used_extension) 
 		except:
-			os.remove(each_dir)			    
+			os.remove(each_dir)	
+
+# FIR seires episode renaming logic is implemented
+
+def rename_FIR(series_selected):
+	webseries_file = os.path.join('Subtitles',series_selected)
+	director_list = os.listdir(webseries_file)
+	for each_dir in director_list:
+		try:
+			# seprating using regular expression
+			name_in_parts = re.findall(r'\d+',each_dir)
+			# segregating season and episode number
+			episode_number = (name_in_parts[0]).strip()
+			# season particular have been restored
+			used_extension = (re.split(r'\.',each_dir)[-1]).strip()
+
+			webseries_file = os.path.join(os.getcwd(),webseries_file)
+			while(len(episode_number) < int(selected_episode)):
+				episode_number = '0' + episode_number
+
+			os.chdir(webseries_file)
+			# renaming of file is done here
+			os.rename(each_dir,'FIR-' + ' Episode ' + episode_number + '.' + used_extension) 
+		except:
+			os.remove(each_dir) 
+
 
 # webseries to be choose, list of webseries is displayed
 webseries_choice = input("Webseries we have in our system:\n1. FIR {FIR}\n2. Game of Thrones {GOT}\n3. How I Met Your Mother {HIMYM}\n4. Sherlock {SLK}\n5. Suits {SUT}\nEnter the code of the webseries which you want ot rename {written inside curly braces}: \n")
@@ -100,7 +125,9 @@ if webseries_choice == "HIMYM":
 elif webseries_choice == "SUT":
 	rename_Suits(season['SUT'])
 elif webseries_choice == "SLK":
-	rename_Sherlock(season['SLK'])	
+	rename_Sherlock(season['SLK'])
+elif webseries_choice == "FIR":
+	rename_FIR(season['FIR'])	
 else:
     print(f"\nwarning:  make sure to write the code of the webseries as given. \ninvalid choice!! webseires with code '{webseries_choice}' doesn't exist in our system.")    
 
