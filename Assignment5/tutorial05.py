d2cef0f2dd5ef817c8184e410e1baab0f53115b9
Nikhil Kumar <nk14646@gmail.com>
2020-11-16 20:27:06 +0530

    Sherlock Homes renamer is added, Have a look!!

--- Assignment5/tutorial05.py
+++ Assignment5/tutorial05.py
@@ -9,9 +9,12 @@ def rename_HIMYM(series_selected):
 	director_list = os.listdir(webseries_file)
 	for each_dir in director_list:
 		try:
+			# seprating using regular expression
 			name_in_parts = re.findall(r'\d+',each_dir)
+			# segregating season and episode number
 			episode_number = (name_in_parts[1]).strip()
 			season_number = (name_in_parts[0]).strip()
+			# season particular have been restored
 			name_in_parts = re.split('-',each_dir)
 			name_of_episode = (re.split(r'\.',name_in_parts[2])[0]).strip()
 			used_extension = (re.split(r'\.',each_dir)[-1]).strip()
@@ -21,6 +24,7 @@ def rename_HIMYM(series_selected):
 			while(len(season_number) < int(selected_season)):
 				season_number = '0' + season_number
 			os.chdir(webseries_file)
+			# renaming of file is done here
 			os.rename(each_dir,'How I Met Your Mother - Season '+ season_number + ' Episode ' + episode_number + '- ' + name_of_episode + '.' + used_extension) 
 		except:
 			os.remove(each_dir)			
@@ -32,9 +36,12 @@ def rename_Suits(series_selected):
 	director_list = os.listdir(webseries_file)
 	for each_dir in director_list:
 		try:
+			# seprating using regular expression
 			name_in_parts = re.findall(r'\d+',each_dir)
+			# segregating season and episode number
 			episode_number = (name_in_parts[1]).strip()
 			season_number = (name_in_parts[0]).strip()
+			# season particular have been restored
 			name_in_parts = re.split('-',each_dir)
 			name_of_episode = (re.split(r'\.',name_in_parts[2])[0]).strip()
 			used_extension = (re.split(r'\.',each_dir)[-1]).strip()
@@ -44,9 +51,35 @@ def rename_Suits(series_selected):
 			while(len(season_number) < int(selected_season)):
 				season_number = '0' + season_number
 			os.chdir(webseries_file)
+			# renaming of file is done here
 			os.rename(each_dir,'Suits - Season '+ season_number + ' Episode ' + episode_number + '- ' + name_of_episode + '.' + used_extension)    
 		except:
-			os.remove(each_dir)    
+			os.remove(each_dir)
+
+# Sherlock!! seires episode renaming logic is implemented
+
+def rename_Sherlock(series_selected):
+	webseries_file = os.path.join('Subtitles',series_selected)
+	director_list = os.listdir(webseries_file)
+	for each_dir in director_list:
+		try:
+			# seprating using regular expression
+			name_in_parts = re.findall(r'\d+',each_dir)
+			# segregating season and episode number
+			season_number = (name_in_parts[0]).strip()
+			episode_number = (name_in_parts[1]).strip()
+			# season particular have been restored
+			used_extension = (re.split(r'\.',each_dir)[-1]).strip()
+			webseries_file = os.path.join(os.getcwd(),webseries_file)
+			while(len(episode_number) < int(selected_episode)):
+				episode_number = '0' + episode_number
+			while(len(season_number) < int(selected_season)):
+				season_number = '0' + season_number	
+			os.chdir(webseries_file)
+			# renaming of file is done here
+			os.rename(each_dir,'Sherlock- Season '+ season_number + ' Episode ' + episode_number + '.' + used_extension) 
+		except:
+			os.remove(each_dir)			    
 
 # webseries to be choose, list of webseries is displayed
 webseries_choice = input("Webseries we have in our system:\n1. FIR {FIR}\n2. Game of Thrones {GOT}\n3. How I Met Your Mother {HIMYM}\n4. Sherlock {SLK}\n5. Suits {SUT}\nEnter the code of the webseries which you want ot rename {written inside curly braces}: \n")
@@ -66,6 +99,8 @@ if webseries_choice == "HIMYM":
 	rename_HIMYM(season['HIMYM'])
 elif webseries_choice == "SUT":
 	rename_Suits(season['SUT'])
+elif webseries_choice == "SLK":
+	rename_Sherlock(season['SLK'])	
 else:
     print(f"\nwarning:  make sure to write the code of the webseries as given. \ninvalid choice!! webseires with code '{webseries_choice}' doesn't exist in our system.")    
 
