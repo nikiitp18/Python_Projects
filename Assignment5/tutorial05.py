561b2ee1e2f52cbd9ce144ee6bc57ab3a34ffe69
Nikhil Kumar <nk14646@gmail.com>
2020-11-16 20:19:30 +0530

    SUITS webseries renamer ia added in our packet. Check it OUT!!

--- Assignment5/tutorial05.py
+++ Assignment5/tutorial05.py
@@ -2,6 +2,8 @@
 import re
 import os 
 
+# How I Met Your Mother!! seires episode renaming logic is implemented
+
 def rename_HIMYM(series_selected):
 	webseries_file = os.path.join('Subtitles',series_selected)
 	director_list = os.listdir(webseries_file)
@@ -23,6 +25,28 @@ def rename_HIMYM(series_selected):
 		except:
 			os.remove(each_dir)			
 
+# Suits!! seires episode renaming logic is implemented
+
+def rename_Suits(series_selected):
+	webseries_file = os.path.join('Subtitles',series_selected)
+	director_list = os.listdir(webseries_file)
+	for each_dir in director_list:
+		try:
+			name_in_parts = re.findall(r'\d+',each_dir)
+			episode_number = (name_in_parts[1]).strip()
+			season_number = (name_in_parts[0]).strip()
+			name_in_parts = re.split('-',each_dir)
+			name_of_episode = (re.split(r'\.',name_in_parts[2])[0]).strip()
+			used_extension = (re.split(r'\.',each_dir)[-1]).strip()
+			webseries_file = os.path.join(os.getcwd(),webseries_file)
+			while(len(episode_number) < int(selected_episode)):
+				episode_number = '0' + episode_number
+			while(len(season_number) < int(selected_season)):
+				season_number = '0' + season_number
+			os.chdir(webseries_file)
+			os.rename(each_dir,'Suits - Season '+ season_number + ' Episode ' + episode_number + '- ' + name_of_episode + '.' + used_extension)    
+		except:
+			os.remove(each_dir)    
 
 # webseries to be choose, list of webseries is displayed
 webseries_choice = input("Webseries we have in our system:\n1. FIR {FIR}\n2. Game of Thrones {GOT}\n3. How I Met Your Mother {HIMYM}\n4. Sherlock {SLK}\n5. Suits {SUT}\nEnter the code of the webseries which you want ot rename {written inside curly braces}: \n")
